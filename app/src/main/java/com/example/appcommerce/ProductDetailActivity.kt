d9fddc441ee5703a95e0a8ca37cdff929abefefc
RubensZaes <rubenszaes@gmail.com>
2020-07-01 20:44:35 -0300

    Etapa 3 - Design da Tela de Produto
    AndroidPro
    Projeto AppCommerce: Etapa 3 - Design da Tela de Produto

---
+++ app/src/main/java/com/example/appcommerce/ProductDetailActivity.kt
@@ -0,0 +1,15 @@
+package com.example.appcommerce
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class ProductDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
